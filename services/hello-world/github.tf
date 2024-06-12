40c668af38c3d06801501a9bbf444338d913db7a
Sam Ainsworth <sam.ainsworth@ukhsa.gov.uk>
2024-06-12 13:12:22 +0100

    Update github.tf

--- services/hello-world/github.tf
+++ services/hello-world/github.tf
@@ -1,15 +1,23 @@
 module "github_application" {
   source                  = "git::https://github.com/ntsedemoorg/terraform-github-repo.git"
 
-  name = "hello-world"
-  description = "Application code for the hello-world service"
+  name = "hello-world-front"
+  description = "Application code for hello-world-front in the hello-world project"
 }
+
+module "github_application" {
+  source                  = "git::https://github.com/ntsedemoorg/terraform-github-repo.git"
+
+  name = "hello-world-api"
+  description = "Application code for hello-world-api in the hello-world project"
+}
+
 
 module "github_infrastructure" {
   source                  = "git::https://github.com/ntsedemoorg/terraform-github-repo.git"
 
   name = "hello-world-infra"
-  description = "Infrastructure code for the hello-world service"
+  description = "Infrastructure code for the hello-world project"
 }
 
 import {  
